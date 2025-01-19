ae75467a8789ee50b68ca388572842f3ad5e8a02
Teresa <akinbodt@mcmaster.ca>
2025-01-19 17:40:28 -0500

    another Footer casing fix up

--- fly-writes/src/pages/BlogPostEntries.jsx
+++ fly-writes/src/pages/BlogPostEntries.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { blogPosts } from "../BlogPostData";
-import Footer from "../footer";
+import Footer from "../Footer";
 
 // this contains all the blog post data for each post (title, subheading, body etc.)
 
